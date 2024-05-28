5631ba4c98ab9f59555a15b80620e07f01e2fe87
vitorvieirah <vitorhvvieira@gmail.com>
2024-05-27 22:26:30 -0300

    Vendo mais sobre o fetch e treinando um pocuo

--- api/front-end/js/main.js
+++ api/front-end/js/main.js
@@ -1,10 +1,23 @@
 const URL = 'http://localhost:3000'
 
 function todos(){
-    let path = `${URL}/nomes`;
+    let path = `${URL}/lanches`;
 
     fetch(path)
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(error => console.log(error));
+}
+
+
+function consultarPorId(){
+    let id = document.getElementById('input-consulta').value;
+
+    let path = `${URL}/lanches/${id}`;
+
+    fetch(path)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+
 }
