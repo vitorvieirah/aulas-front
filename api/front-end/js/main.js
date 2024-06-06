bed6c55ab52c84e7f5b0f26b60f3295478bec324
vitorvieirah <vitorhvvieira@gmail.com>
2024-06-06 20:32:44 -0300

    Finalizando os metodos do HTTP

--- api/front-end/js/main.js
+++ api/front-end/js/main.js
@@ -1,4 +1,12 @@
+// function  clickButtonGet(){
+//     document.getElementById
+// }
+
+
 const URL = 'http://localhost:3000/lanches'
+let nameInput;
+let idInput;
+
 
 function todos(){
     let path = URL;
@@ -8,6 +16,21 @@ function todos(){
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(error => console.log(error));
+}
+
+
+function todosV2(){
+    parametros = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    fetch(URL, parametros)
+        .then(response => response.json())
+        .then(objs => console.log("objs: ", objs))
+        .catch(err => console.log("Erro:", err))
 }
 
 
@@ -23,13 +46,17 @@ function consultarPorId(){
 
 }
 
+function consultarPorIdV2(){
+
+}
+
 function criarLanche(){
-    criarNovoLanche(JSON.stringify(novoLancheXtudo()));
+    criarNovoLanche(novoLancheXtudo());
 }
 
 function novoLancheXtudo() {
     return {
-        "id": "1",
+        "id": "20",
         "nome": "X-Tudo",
         "preco": 30.5,
         "descricao": "O melhor X-Tudp da região",
@@ -53,14 +80,53 @@ function criarNovoLanche(novoLanche){
         headers: {
             "Content-type": "application/json"
         },
-        body: {
-            novoLanche
-        }
+        body: JSON.stringify(novoLanche)
     };
 
 
     fetch(URL, parametros)
-    .then(response => response.json())
-    .then(lanche => console.log("Lanche: ", lanche))
-    .catch((err) => console.log("erro", err));
+        .then(response => response.json())
+        .then(lanche => console.log("Lanche: ", lanche))
+        .catch(err => console.log("erro", err));
+}
+
+function getPararms(){
+    idInput = document.getElementById('id-put').value;
+    nameInput = document.getElementById('input-name').value;
+}
+
+function editarLanche(){
+    getPararms();
+    bodyRequest = {
+        nome: nameInput
+    };
+
+    let parametros = {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(bodyRequest)
+    }
+
+    fetch(`${URL}/${idInput}`, parametros)
+        .then(response => response.json())
+        .then(lanche => console.log("Lanche alterado", lanche))
+        .catch(err => console.log("Erro", err));
+}
+
+function excluirLanche(){
+    getPararms();
+
+    let parametros = {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
+
+    fetch(`${URL}/${idInput}`, parametros)
+        .then(response => response.json())
+        .then(lanche => console.log("Lanche: ", lanche))
+        .catch(err => console.log("erro", err));
 }
